4bf779f7d54aa43ba51d5181e3c69133d3ec30c4
vlad <vkharkivsk>
2022-08-06 15:54:48 +0300

    Ex4

--- task3/ex4/script.js
+++ task3/ex4/script.js
@@ -1,1 +1,36 @@
+function drawTriangle() {
 
+  this.canvas = document.createElement('canvas');
+  this.canvas.setAttribute('width' ,'100px');
+
+  if (this.canvas.getContext) {
+    this.ctx = this.canvas.getContext('2d');
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(75,75);
+    this.ctx.lineTo(10,75);
+    this.ctx.lineTo(10,25);
+    this.ctx.fill();
+
+    document.body.appendChild(this.canvas);
+  }
+}
+
+function drawSquare() {
+    
+    this.canvas = document.createElement('canvas');
+    this.canvas.setAttribute('width' ,'100px');
+    
+    if (this.canvas.getContext) {
+        this.ctx = this.canvas.getContext('2d');
+
+        this.ctx.fillRect(20, 10, 90, 90);
+
+        document.body.appendChild(this.canvas);
+    }
+}
+
+let triangle  = new drawTriangle();
+let square = new drawSquare();
+let triangle2  = new drawTriangle();
+let square2 = new drawSquare();
