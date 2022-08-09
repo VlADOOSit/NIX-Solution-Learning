33f7837c0b886bd9cef94441645a2b054c554e36
vlad <vlad.kharkovskiy10@gmail.com>
2022-08-09 13:52:27 +0300

    Ex4

--- task4/ex4/script.js
+++ task4/ex4/script.js
@@ -1,1 +1,11 @@
-console.log('ex4');
+function testDate(str) {
+    let re = /^([1-2]\d|0?[1-9]|3[0-1])-(0?[1-9]|1[0-2])-(\d{4})$/;
+
+    return re.test(str);
+}
+
+console.log(testDate('25-5-2021'));//true
+console.log(testDate('25-5-1921'));//true
+console.log(testDate('2-25-2021'));//false
+console.log(testDate('213-5-2021'));//false
+console.log(testDate('2-5-2021'));//true
